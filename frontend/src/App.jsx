b380b6d23d1ff00e6da72300f090f301f862cfe9
SantiagoAriasR <santiago.ariasr1@uqvirtual.edu.co>
2026-05-08 21:19:39 -0500

    Agregar página Visitantes con listado y formulario de registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
+import Visitantes from "./pages/Visitantes"
 import "./App.css"
 
 function App() {
@@ -9,11 +10,13 @@ function App() {
                 <span className="nav-brand">🎢 Tech-Park UQ</span>
                 <div className="nav-links">
                     <Link to="/">Inicio</Link>
+                    <Link to="/visitantes">Visitantes</Link>
                 </div>
             </nav>
 
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/visitantes" element={<Visitantes />} />
             </Routes>
         </div>
     )
