a2ef16fcfefdb496580726ebee9ac677f34eb33f
SantiagoAriasR <santiago.ariasr1@uqvirtual.edu.co>
2026-05-10 20:18:51 -0500

    Quitar imports no usados

--- backend/src/main/java/com/techpark/controller/VisitanteController.java
+++ backend/src/main/java/com/techpark/controller/VisitanteController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import com.techpark.estructuras.Nodo;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/visitantes")
